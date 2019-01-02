f1bbcc99311a5b12c03d05bda9a933f78f61de30
MarceloBD <marcelodianib@gmail.com>
2019-01-02 10:45:39 -0200

    Specifications done

--- part2/smartclassifier/extract_info.py
+++ part2/smartclassifier/extract_info.py
@@ -1,11 +1,30 @@
 from file_manager import File_manager
-import numpy as np
+
 
 TEST_FILENAME = "results.tsv"
 MAX_RAM_DIGITS = 10
-COLORS = ['azul', 'verde', 'preto', 'branco', 'prata', 'platinum', 'indigo', 'dourado', 'ouro', 'cinza', 'índigo']
+COLORS = ['azul topázio', 'azul', 'verde', 'preto', 'branco', 'prata', 'platinum', 'indigo', 'dourado','ouro rosé',
+		  'ouro', 'cinza', 'índigo', 'rose gold', 'rose', 'titanium', 'ametista', 'rosa', 'gold', 
+		  'vermelho', 'ultravioleta', 'black', 'titânio', 'red', 'violeta']
 MAX_SIZE_DIGITS = 10
-STOP_WORDS = ["com", "tela"]
+STOP_WORDS = ["com", "tela", "android", "-"]
+INSIDE_STOP_WORDS = [",", "."]
+SIDE_STOP_WORDS = ['+']
+
+def is_valid_ram(ram):
+	if(len(ram) != 0 and (ram[-1]=='b' and ram[-2]=='g')):
+		for c_i in range(0, len(ram)-2):
+			if(not ram[c_i].isdigit()):
+				return False
+		return True
+	elif(len(ram) != 0 and ram[-1] =='g'):
+		for c_i in range(0, len(ram)-1):
+			if(not ram[c_i].isdigit()):
+				return False
+		return True
+	return False
+
+
 
 def is_word_in_string(string):
 	for word in STOP_WORDS+COLORS:
@@ -14,35 +33,61 @@ def is_word_in_string(string):
 				if( string[i] == " "):
 					break
 				else:
-					
 					string = string[:i]
-					print(string)
 			return [True, string]
+	
+	for word in INSIDE_STOP_WORDS:
+		if(word in string.lower()):
+			for i in range(len(string)-2, 0, -1):
+				if( string[i] == " "):
+					break
+				else:
+					string = string[:i]
+			return [True, string]
+	for word in SIDE_STOP_WORDS:
+		if(' '+word in string.lower()):
+			for i in range(len(string)-2, 0, -1):
+				if( string[i] == " "):
+					break
+				else:
+					string = string[:i]
+			return [True, string]
+		
+	ram = ''
+	for c_i in range(len(string)-2, 0, -1):
+		if(string[c_i] == " "):
+			break
+		else:
+			ram += string[c_i]
+	if(is_valid_ram(ram[::-1].lower())):
+		for i in range(len(string)-2, 0, -1):
+			if( string[i] == " "):
+				break
+			else:
+				string = string[:i]
+		return [True, string]
 	return [False, string]
-
+	
 def get_data_from_title(smartphone):
 	title = smartphone[1]
 	i_title = 0
 	specs = []
 	name = ""
 	for i in range(len(title)):
-		if(title[i].isdigit() and i > 0 and title[i-1] == " "):
-			i_title = i 
-			break
-		else:
-			name += title[i].lower()
-			if(title[i] == " " or title[i]==','):
-				[ended, name] = is_word_in_string(name)
-				if(ended):
-					break
+		name += title[i].lower()
+		if(title[i] == " " or title[i]==','):
+			[ended, name] = is_word_in_string(name)
+			if(ended):
+				break
 	
 	specs += [name]
 	
 	
 	ram = ""
+	i_ram = 0
 	for i in range(i_title, len(title)):
-		#print(title[i])
 		if(i+2 < len(title) and title[i].lower() == 'r' and title[i+1].lower() == 'a' and title[i+2].lower()== 'm'):
+			i_ram = i+4 
 			for not_numbers in range(i-2, i-MAX_RAM_DIGITS, -1):
 				if(title[not_numbers].isdigit()):
 					break
@@ -56,7 +101,27 @@ def get_data_from_title(smartphone):
 				else:
 					break
 			break
-	specs += [ram[::-1]]
+	ram = ram[::-1].lower()
+	
+	if(not is_valid_ram(ram)):
+		ram = ''
+		for not_numbers in range(i_ram, i_ram+MAX_RAM_DIGITS):
+			if(not_numbers >= len(title) or title[not_numbers].isdigit()):
+				break
+			elif(title[not_numbers] == " "):
+				i_ram = not_numbers+1
+				break
+		for j in range(i_ram, i_ram+MAX_RAM_DIGITS):
+			if(j < len(title) and title[j] != " "):
+				ram += title[j]
+			else:
+				break
+	ram = ram.lower()
+	if(not is_valid_ram(ram)):
+		ram = ''
+	if(len(ram) and ram[-1] == 'g'):
+		ram += 'b'
+	specs += [ram]
 		
 	col = ""
 	for color in COLORS:
@@ -68,24 +133,43 @@ def get_data_from_title(smartphone):
 	
 	size = ""
 	for i in range(len(title)):
-		if(title[i] == "'" or title[i] == '"'):
-			for j in range(i, i-MAX_SIZE_DIGITS, -1):
+
+		if(title[i] == "'" or title[i] == '"' or title[i] == '”'):
+			for j in range(i-1, i-MAX_SIZE_DIGITS, -1):
 				if(title[j] == " "):
 					break
 				size += title[j]
+			size = size[::-1]
 			break
 		if(title[i] == '.' or title[i] == ','):
+			point = False
 			if(not title[i-1].isdigit() or not title[i+1].isdigit()):
-				break
+				continue
 			while(title[i] != " "):
 				i -= 1
 			
 			for j in range(i+1, i+MAX_SIZE_DIGITS):
 				if(j >= len(title)):
 					break
-				if(title[j] == " "):
+				if(title[j] == " "  or title[j] == '”' or title[j] == "'" or title[j] == '"'):
 					break
+				elif(title[j] == '.' or title[j] == ',' or not title[j].isdigit()):
+					if(point):
+						break
+					else:
+						if(title[j] != '.' and title[j] != ',' and not title[j].isdigit()):
+							size = ''
+							break
+						else:
+							point = True
 				size += title[j]
+			break
+	#print(title)
+	if(len(size) > 5):
+		size = ''
+	for c_i in range(len(size)):
+		if(size[c_i] == '.'):
+			size = size[:c_i]+','+size[c_i+1:]
 			break
 	specs += [size]
 	smartphone += [specs]
